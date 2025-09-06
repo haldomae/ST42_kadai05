9ab32287a4fba61de5df9e22ba16c6692c5b215a
haldomae <hal.domae@gmail.com>
2025-09-06 15:22:07 +0900

    状態管理のクラスを作成

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,24 @@ class Chinchiro extends StatelessWidget {
   }
 }
 
+// チンチロゲームの画面（状態が変わるのでStatefulWidget）
+// 設計図クラス(StatefulWidget)
+// 「チンチロゲーム画面を作って」という命令書
+// 「状態管理クラスのインスタンス（実体）を作って返す」
+class ChinchiroGame extends StatefulWidget{
+  @override
+  _ChinchiroGameState createState() => _ChinchiroGameState();
+}
+
+// チンチロゲームの状態を管理するクラス(設計図クラス(StatefulWidget))
+// 実際の状態管理クラス(State)
+// 実際の状態とデータを管理する作業員
+// _がある = プライベート(秘密)
+// _がない = パブリック(公開)
+class _ChinchiroGameState extends State<ChinchiroGame>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
