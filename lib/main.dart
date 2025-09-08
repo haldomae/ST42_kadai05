de0552eb0e4e93825ba6d2006b080078a4a5ad81
haldomae <hal.domae@gmail.com>
2025-09-08 10:16:18 +0900

    アイコンと文字列を表示

--- lib/main.dart
+++ lib/main.dart
@@ -36,13 +36,37 @@ class HomePage extends StatelessWidget{
         ),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Text(
-          "Hello Flutter",
-          // 装飾
-          style: TextStyle(
-              fontSize: 24,
-              color: Colors.blue
+      // Containerは装飾可能な箱
+      body: Container(
+        // 画面の横幅いっぱい
+        width: double.infinity,
+        // 画面の縦幅いっぱい
+        height: double.infinity,
+        // 子要素配置
+        // 中央に配置
+        child: Center(
+          // 縦並べ
+          child: Column(
+            // 縦方向の中央寄せ
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // アイコン表示
+              Icon(
+                Icons.flutter_dash,
+                size: 100,
+                color: Colors.red,
+              ),
+              // 間を開ける
+              SizedBox(height: 20),
+              Text(
+                "Flutterの鳥",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber
+                ),
+              ),
+            ],
           ),
         ),
       ),
