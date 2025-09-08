d1b79dee7b5d7ef5743f4a2b3a387968d35f6588
haldomae <hal.domae@gmail.com>
2025-09-08 09:48:55 +0900

    メイン部分を別クラスに分割

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,28 @@ class MyApp extends StatelessWidget {
     // MaterialAppはアプリ全体の設定
     return MaterialApp(
       title: "Flutterのテスト",
-      // Scaffoldは画面の基本構成
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("自分のアプリ"),
-        ),
-        body: Center(
-          child: Text(
-            "Hello Flutter",
-            // 装飾
-            style: TextStyle(
-                fontSize: 24,
-                color: Colors.blue
-            ),
+      // メインのページ
+      home: HomePage()
+    );
+  }
+}
+
+// ホーム画面専用のクラス
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // Scaffoldは画面の基本構成
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("自分のアプリ"),
+      ),
+      body: Center(
+        child: Text(
+          "Hello Flutter",
+          // 装飾
+          style: TextStyle(
+              fontSize: 24,
+              color: Colors.blue
           ),
         ),
       ),
